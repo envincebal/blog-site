024baf335434efd472754e9330ac80a558674650
envincebal <vincentyan1@gmail.com>
2019-02-21 22:26:12 -0800

    Turned posts elements into array

--- public/js/date.js
+++ public/js/date.js
@@ -1,8 +1,13 @@
-let dateFormat = document.querySelector(".post-date");
+let dateFormat = document.getElementsByClassName("post-date");
+// Turns date elements into array
+let dateArr = Array.prototype.slice.call(dateFormat);
 
-if (dateFormat !== null) {
-  let dateText = dateFormat.textContent;
+// Loops through posts and formats them based on local time and date
+dateArr.forEach(date => {
+  let dateText = date.textContent;
+  console.log(dateText);
   let localDate = new Date(dateText).toLocaleString('en-US');
-  dateFormat.textContent = localDate;
 
-}
+  date.textContent = localDate;
+});
+
