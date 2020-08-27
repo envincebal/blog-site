12450a2df09309fbe6e2ea4d065528fa99cba807
envincebal <vincentyan1@gmail.com>
2020-08-27 01:59:05 -0700

    Removed authentication.

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-const session = require("express-session");
-const user = require("./routes/users");
-const passport = require("passport");
-require("./passport");
-const bcrypt = require("bcrypt");
-const postModel = require("./models/posts");
-const userModel = require("./models/users");
-
 const methodOverride = require("method-override");
-
-const Posts = postModel.Post;
-const Users = userModel.User;
-
 const postsRouter = require("./routes/posts");
 
-
-mongoose.connect(process.env.CONNECTION_URI, {
+mongoose.connect(process.env.CONNECTION_URI || "mongodb://localhost:27017/blogDB", {
   useUnifiedTopology: true,
   useNewUrlParser: true 
 });
@@ -27,56 +13,12 @@ let app = express();
 
 app.set("view engine", "ejs");
 
-app.use(session({
-  secret: "cats",
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(function (req, res, next) {
-  res.locals.currentUser = req.user;
-  next();
-});
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
-
-app.get("/sign-up", (req, res) => res.render("sign-up-form"));
-
-app.post("/sign-up", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    const user = new Users({
-      username: req.body.username,
-      password: hashedPassword
-    }).save(err => {
-      if (err) {
-        return next(err);
-      } else {
-        res.redirect("/");
-      }
-    });
-  });
-});
-
-app.post("/log-in", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/"
-}));
-
-app.get("/log-out", (req, res) => {
-  req.logout();
-  res.redirect("/");
-});
-
-
-app.get("/log-out", (req, res) => {
-  req.logout();
-  res.redirect("/");
-});
 
 app.use("/", postsRouter);  
 
--- controllers/postController.js
+++ controllers/postController.js
@@ -3,14 +3,14 @@ const Posts = postModel.Post;
 
 module.exports = {
   index: (req, res) => {
- 
+
     let perPage = 3;
     let totalItems;
     let currentPage = parseInt(req.query.page) || 1;
     Posts.find()
       .countDocuments()
       .then(count => {
-  
+
         totalItems = count;
         const lastPage = Math.ceil(totalItems / perPage);
         Posts.find()
@@ -20,7 +20,7 @@ module.exports = {
           .skip((currentPage - 1) * perPage)
           .limit(perPage)
           .then(posts => {
-  
+
             res.render("home", {
               user: req.user,
               posts,
@@ -31,21 +31,21 @@ module.exports = {
           });
       })
   },
-  
+
   compose_get: (req, res) => res.render("compose"),
 
   compose_post: (req, res) => {
     const postTitle = req.body.postTitle;
     const postBody = req.body.postBody;
-  
+
     let postDate = new Date();
-  
+
     const post = new Posts({
       date: postDate,
       title: postTitle,
       content: postBody
     });
-  
+
     post.save(err => {
       if (!err) {
         res.redirect("/");
@@ -71,11 +71,11 @@ module.exports = {
 
   edit_get_id: (req, res) => {
     const requestedId = req.params.id;
-  
+
     Posts.findOne({
       _id: requestedId
     }, (err, post) => {
-  
+
       if (!err) {
         res.render("edit", {
           title: post.title,
@@ -92,9 +92,9 @@ module.exports = {
       title: req.body.postTitle,
       content: req.body.postBody
     };
-   
+
     Posts.findByIdAndUpdate(req.params.id, postEdit, (err, post) => {
-  
+
       if (err) {
         console.log(err);
       } else {
@@ -105,12 +105,12 @@ module.exports = {
 
   delete_post: (req, res) => {
     const deletePosts = req.body.delete;
-  
+
     Posts.findByIdAndRemove(deletePosts, (err) => {
       if (err) {
         console.log(err);
       } else {
-        res.redirect("back");
+        res.redirect("/");
       }
     });
   }
